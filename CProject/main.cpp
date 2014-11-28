2c378fceff5049a637b62067df10adc305759c71
Sanika Kulkarni <sanikakulkarni@Sanikas-MacBook-Pro.local>
2014-11-28 17:33:19 -0500

    Initial Commit

---
+++ CProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CProject
+//
+//  Created by Sanika Kulkarni on 11/28/14.
+//  Copyright (c) 2014 Sanika Kulkarni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
